0b7394686ca9b5dc1ff9d0766d48175438f9e3de
olujide2000 <olujide2000@gmail.com>
2023-02-03 18:26:38 +0100

    Adding second activity

---
+++ GraduationApp/app/src/main/java/com/olujide/graduationapp/GraduationActivity.kt
@@ -0,0 +1,11 @@
+package com.olujide.graduationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GraduationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graduation)
+    }
+}
--- GraduationApp/app/src/main/java/com/olujide/graduationapp/MainActivity.kt
+++ GraduationApp/app/src/main/java/com/olujide/graduationapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.olujide.graduationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
+        val btnList = findViewById<Button>(R.id.btn_list)
+
+        btnSubmit.setOnClickListener {
+            startActivity(Intent(this, GraduationActivity::class.java))
+        }
+
+        btnList.setOnClickListener {
+            startActivity(Intent(this, ListActivity::class.java))
+        }
+
     }
 }
